616034c1d6b5186519b9b2ccff59cc772b781d35
DamianRz <damianezetiel1@gmail.com>
2020-06-01 16:50:29 -0300

    fixes in page

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
-import React, { ReactElement, FC } from 'react';
-import '../styles/theme.scss';
-const App: FC<{ Component: any; pageProps: any }> = ({
-  Component,
-  pageProps,
-}) => <Component {...pageProps} />;
-export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { LoginForm } from '../components/LoginForm/index';
 import { RegisterForm } from '../components/RegisterForm/index';
 import { FormProvider } from '../contexts/FormContext';
+import { Button } from '../components/Button';
+import './index.scss';
+
 const IndexPage = () => {
   const [login, setLogin] = useState(true);
-  if (login)
-    return (
-      <div className="background-page">
-        <FormProvider values={{ username: '', password: '' }}>
+
+  const AccountBox = () => {
+    if (login) {
+      return (
+        <Fragment>
           <LoginForm />
-        </FormProvider>
-        <p className="register-label">If you not have a account</p>
-        <button onClick={() => setLogin(false)}>Register</button>
-      </div>
-    );
+          <div className="account-footer">
+            <p className="info-label">If you not have a account</p>
+            <Button
+              width="150px"
+              label="Register Here"
+              onClick={() => setLogin(false)}
+            />
+          </div>
+        </Fragment>
+      );
+    } else {
+      return (
+        <Fragment>
+          {/* <FormProvider fields={{ username: '', email: '', password: '' }}>
+            <RegisterForm />
+          </FormProvider> */}
+          <div className="account-footer">
+            <p className="info-label">If you have a account</p>
+            <Button
+              width="150px"
+              label="Login Here"
+              onClick={() => setLogin(true)}
+            />
+          </div>
+        </Fragment>
+      );
+    }
+  };
+
   return (
     <div className="background-page">
-      <FormProvider values={{ username: '', email: '', password: '' }}>
-        <RegisterForm />
-      </FormProvider>
-      <p className="login-label">If you have a account</p>
-      <button onClick={() => setLogin(true)}>Login</button>
+      <div className="account-box">
+        <h2 className="account-box__title">WeCollab</h2>
+        <AccountBox />
+      </div>
     </div>
   );
 };
-// return (
-//   <Fragment>
-//     <FormProvider values={{ username: '', password: '' }}>
-//       <LoginForm className={!loginState ? 'hide' : ''} />
-//     </FormProvider>
-//     <FormProvider values={{ username: '', password: '', email: '' }}>
-//       <RegisterForm className={loginState ? 'hide' : ''} />
-//     </FormProvider>
-//   </Fragment>
-// );
+
 IndexPage.displayName = 'Index Page';
 export default IndexPage;
