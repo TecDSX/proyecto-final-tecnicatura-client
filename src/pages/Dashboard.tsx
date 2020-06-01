c358a3eeb7c0fc1403016de9e2e5090d335bcbc3
DamianRz <damianezetiel1@gmail.com>
2020-06-01 16:50:16 -0300

    add page Dashboard

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { LeftMenu } from '../components/LeftMenu';
+import { Toolbar } from '../components/Toolbar';
+import './Dashboard.scss';
+
+
+const DashboardPage = () => {
+  return (
+    <Fragment>
+      <Toolbar />
+      <div className="page-box">
+        <LeftMenu />
+        <div className="dashboard">
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+DashboardPage.displayName = 'Dashboard Page';
+export default DashboardPage;
